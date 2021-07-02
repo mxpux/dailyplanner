43dfd3fd0e699c8378b1e07a047b4bfd54a27ec0
MPx <hellomariap@gmail.com>
2021-07-01 20:38:06 -0700

    Fixed comments and typos

--- assets/script.js
+++ assets/script.js
@@ -12,15 +12,15 @@ dateDisplay.text(currentTime + " " + todayDay + ", " + currentDate)
 // Get the current hour via Moment JS
 var currentHour = moment().hour();
 
-// check the timeblock availability
+// Check the timeblock availability
 // by using loop, this method will check if the current time and 
-// the timeblock availability value 
+// the timeblock availability value.
   $(".time-block").each(function() {
        var checkHour = parseInt($(this).attr("id"));
-      // by using remove class, it will set the colors to the exact timeblock and current time
+      // By using remove class, it will set the colors to the exact timeblock and current time
        $(this).removeClass("past present future");
    
-  // As the above stated, now the statement below will determine and
+  // As the above stated, the statement below will determine 
   // to see or "check" the hour if it matches the current hour.
   // If the condition matches, it will add the class the "current hour" belong to.
   if (checkHour < currentHour) {    
@@ -35,15 +35,15 @@ var currentHour = moment().hour();
 });
 
   $(".saveBtn").on("click", function () {
-  // save Agenda to local storage
+  // Save Agenda to local storage
   saveAgenda = $(this).siblings(".formControl").val();
   console.log(saveAgenda);
-  // save time-block to local storage
+  // Save time-block to local storage
   saveTime = $(this).closest(".time-block").attr("id");
   console.log(saveTime);
   localStorage.setItem(saveTime, saveAgenda);
 
-  // Text area and form control to see the saved Agenda item in Console
+  // Text area and form control to see the saved Agenda items in Local Storage
   $("9 .formControl").val(localStorage.getItem("9"));
   $("10 .formControl").val(localStorage.getItem("10"));
   $("11 .formControl").val(localStorage.getItem("11"));
